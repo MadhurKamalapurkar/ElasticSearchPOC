182756342e07fcf55301dca6339ca459ae54f35a
Madhur <kmadhur21@gmail.com>
2018-05-16 01:18:27 -0700

    tabs to spaces

--- src/main/java/com/amazonaws/lambda/demo/DataIngesterHandler.java
+++ src/main/java/com/amazonaws/lambda/demo/DataIngesterHandler.java
@@ -23,91 +23,92 @@ import io.searchbox.core.Index;
 
 public class DataIngesterHandler implements RequestHandler<S3Event, String> {
 
-	private AmazonS3 s3 = AmazonS3ClientBuilder.standard().build();
-	private ObjectMapper mapper = new ObjectMapper();
-	private JestClientFactory factory = new JestClientFactory();
-	// Removed URL for safety purpose
-	private static final String URL = "";
+    private AmazonS3 s3 = AmazonS3ClientBuilder.standard().build();
+    private ObjectMapper mapper = new ObjectMapper();
+    private JestClientFactory factory = new JestClientFactory();
+    // Removed URL for safety purpose
+    private static final String URL = "";
 
-	public DataIngesterHandler() {
-	}
+    public DataIngesterHandler() {
+    }
 
-	// Test purpose only.
-	DataIngesterHandler(AmazonS3 s3) {
-		this.s3 = s3;
-	}
+    // Test purpose only.
+    DataIngesterHandler(AmazonS3 s3) {
+        this.s3 = s3;
+    }
 
-	@Override
-	public String handleRequest(S3Event event, Context context) {
-		context.getLogger().log("Received event: " + event);
+    @Override
+    public String handleRequest(S3Event event, Context context) {
+        context.getLogger().log("Received event: " + event);
 
-		// Get the object from the event and show its content type
-		String bucket = event.getRecords().get(0).getS3().getBucket().getName();
-		String key = event.getRecords().get(0).getS3().getObject().getKey();
-		factory.setHttpClientConfig(new HttpClientConfig.Builder(URL).multiThreaded(true).build());
-		JestClient client = factory.getObject();
-		S3Object response = s3.getObject(new GetObjectRequest(bucket, key));
-		InputStream input = response.getObjectContent();
-		// Read data from CSV file
-		List<Object> readAll = new ArrayList<>();
-		try {
-			CsvSchema csvSchema = CsvSchema.builder().setUseHeader(true).build();
-			CsvMapper csvMapper = new CsvMapper();
-			readAll = csvMapper.readerFor(Map.class).with(csvSchema).readValues(input).readAll();
+        // Get the object from the event and show its content type
+        String bucket = event.getRecords().get(0).getS3().getBucket().getName();
+        String key = event.getRecords().get(0).getS3().getObject().getKey();
+        factory.setHttpClientConfig(new HttpClientConfig.Builder(URL).multiThreaded(true).build());
+        JestClient client = factory.getObject();
+        S3Object response = s3.getObject(new GetObjectRequest(bucket, key));
+        InputStream input = response.getObjectContent();
+        // Read data from CSV file
+        List<Object> readAll = new ArrayList<>();
+        try {
+            CsvSchema csvSchema = CsvSchema.builder().setUseHeader(true).build();
+            CsvMapper csvMapper = new CsvMapper();
+            readAll = csvMapper.readerFor(Map.class).with(csvSchema).readValues(input).readAll();
 
-			List<Index> indexes = new ArrayList<Index>();
-			readAll.parallelStream().forEach(object -> {
-				try {
-					indexes.add(new Index.Builder(mapper.writeValueAsString(object)).build());
-					Index index = new Index.Builder(mapper.writeValueAsString(object)).index("report").type("sales")
-							.build();
-					client.execute(index);
-				} catch (Exception e) {
-					e.printStackTrace();
-					context.getLogger().log(e.getMessage());
-				}
+            List<Index> indexes = new ArrayList<Index>();
+            readAll.parallelStream().forEach(object -> {
+                try {
+                    indexes.add(new Index.Builder(mapper.writeValueAsString(object)).build());
+                    Index index = new Index.Builder(mapper.writeValueAsString(object)).index("report").type("sales")
+                            .build();
+                    client.execute(index);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    context.getLogger().log(e.getMessage());
+                }
 
-			});
+            });
 
-			return "success";
-		} catch (Exception e) {
-			e.printStackTrace();
-			context.getLogger().log(e.getMessage());
-			return "Fail: " + e.getMessage();
-		} finally {
-			if (client != null) {
-				try {
-					client.close();
-				} catch (Exception e) {
-					e.printStackTrace();
-					context.getLogger().log(e.getMessage());
-					return "Fail: " + e.getMessage();
-				}
+            return "success";
+        } catch (Exception e) {
+            e.printStackTrace();
+            context.getLogger().log(e.getMessage());
+            return "Fail: " + e.getMessage();
+        } finally {
+            if (client != null) {
+                try {
+                    client.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    context.getLogger().log(e.getMessage());
+                    return "Fail: " + e.getMessage();
+                }
 
-			}
-		}
-	}
-	
-	/*
+            }
+        }
+    }
+    
+    /*
      * Asynchronous code, cannot be included since lambda is not active once the session is killed
     if (client == null) {
-    	System.out.println("Client is null");
-    	return "Client is null: Fail";
+        System.out.println("Client is null");
+        return "Client is null: Fail";
     } else {
-    	Bulk bulk = new Bulk.Builder().defaultIndex("report_16").defaultType("sales1").addAction(indexes).build();
-    	client.executeAsync(bulk, new JestResultHandler<JestResult>() {
+        Bulk bulk = new Bulk.Builder().defaultIndex("report_16").defaultType("sales1").addAction(indexes).build();
+        client.executeAsync(bulk, new JestResultHandler<JestResult>() {
 
-			@Override
-			public void completed(JestResult result) {
-				System.out.println(result.getJsonString());
-			}
+            @Override
+            public void completed(JestResult result) {
+                System.out.println(result.getJsonString());
+            }
 
-			@Override
-			public void failed(Exception ex) {
-				System.out.println(ex.getMessage());
-			}
-		});
+            @Override
+            public void failed(Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+        });
     }
     */
+
 
 }
--- src/main/java/com/amazonaws/lambda/demo/SearchHandler.java
+++ src/main/java/com/amazonaws/lambda/demo/SearchHandler.java
@@ -23,59 +23,59 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 
 public class SearchHandler implements RequestStreamHandler {
-	private JSONParser parser = new JSONParser();
-	// Removed URL for safety purpose
-	private static final String URL = "";
+    private JSONParser parser = new JSONParser();
+    // Removed URL for safety purpose
+    private static final String URL = "";
 
-	@Override
-	public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
-		JSONObject response = new JSONObject();
-		String q = "";
-		BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-		try {
-			JSONObject event = (JSONObject) parser.parse(reader);
-			context.getLogger().log("query params: " + event.get("queryStringParameters"));
-			if (event.get("queryStringParameters") != null) {
-				JSONObject qps = (JSONObject) event.get("queryStringParameters");
-				if (qps.get("q") != null) {
-					q = (String) qps.get("q");
-				}
-			}
+    @Override
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+        JSONObject response = new JSONObject();
+        String q = "";
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        try {
+            JSONObject event = (JSONObject) parser.parse(reader);
+            context.getLogger().log("query params: " + event.get("queryStringParameters"));
+            if (event.get("queryStringParameters") != null) {
+                JSONObject qps = (JSONObject) event.get("queryStringParameters");
+                if (qps.get("q") != null) {
+                    q = (String) qps.get("q");
+                }
+            }
 
-			context.getLogger().log("query param: " + q);
-			response = callES(q);
+            context.getLogger().log("query param: " + q);
+            response = callES(q);
 
-		} catch (org.json.simple.parser.ParseException e) {
-			response.put("statusCode", 500);
-			response.put("body", e.getMessage());
-			context.getLogger().log(e.getMessage());
-		} catch (Exception e) {
-			response.put("statusCode", 500);
-			response.put("body", e.getMessage());
-			context.getLogger().log(e.getMessage());
-		}
+        } catch (org.json.simple.parser.ParseException e) {
+            response.put("statusCode", 500);
+            response.put("body", e.getMessage());
+            context.getLogger().log(e.getMessage());
+        } catch (Exception e) {
+            response.put("statusCode", 500);
+            response.put("body", e.getMessage());
+            context.getLogger().log(e.getMessage());
+        }
 
-		OutputStreamWriter writer = new OutputStreamWriter(outputStream, "UTF-8");
-		writer.write(response.toJSONString());
-		writer.close();
-	}
+        OutputStreamWriter writer = new OutputStreamWriter(outputStream, "UTF-8");
+        writer.write(response.toJSONString());
+        writer.close();
+    }
 
-	public JSONObject callES(final String query) throws ParseException, IOException {
-		HttpClient httpClient = new DefaultHttpClient();
-		JSONObject responseJson = new JSONObject();
-		String responseString = "";
-		HttpPost httpPost = new HttpPost(URL);
-		httpPost.setHeader("Content-Type", ContentType.APPLICATION_JSON.toString());
-		String body = "{\"query\": {\"multi_match\" : {\"query\":" + "\"" + query + "\""
-				+ ", \"fields\":[\"PLAN_NAME\", \"SPONSOR_DFE_NAME\", \"SPONS_DFE_MAIL_US_STATE\"],\"operator\": \"or\"}}}";
-		StringEntity stringEntity = new StringEntity(body);
-		httpPost.setEntity(stringEntity);
-		HttpResponse httpResponse = httpClient.execute(httpPost);
-		HttpEntity entity = httpResponse.getEntity();
-		responseString = EntityUtils.toString(entity, "UTF-8");
-		responseJson.put("body", responseString);
-		responseJson.put("statusCode", httpResponse.getStatusLine().getStatusCode());
-		return responseJson;
-	}
+    public JSONObject callES(final String query) throws ParseException, IOException {
+        HttpClient httpClient = new DefaultHttpClient();
+        JSONObject responseJson = new JSONObject();
+        String responseString = "";
+        HttpPost httpPost = new HttpPost(URL);
+        httpPost.setHeader("Content-Type", ContentType.APPLICATION_JSON.toString());
+        String body = "{\"query\": {\"multi_match\" : {\"query\":" + "\"" + query + "\""
+                + ", \"fields\":[\"PLAN_NAME\", \"SPONSOR_DFE_NAME\", \"SPONS_DFE_MAIL_US_STATE\"],\"operator\": \"or\"}}}";
+        StringEntity stringEntity = new StringEntity(body);
+        httpPost.setEntity(stringEntity);
+        HttpResponse httpResponse = httpClient.execute(httpPost);
+        HttpEntity entity = httpResponse.getEntity();
+        responseString = EntityUtils.toString(entity, "UTF-8");
+        responseJson.put("body", responseString);
+        responseJson.put("statusCode", httpResponse.getStatusLine().getStatusCode());
+        return responseJson;
+    }
 
 }
